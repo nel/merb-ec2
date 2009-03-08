10d0e762120314bb9e6c57f43b9ad57ed69d2920
Alex Coles <alex@alexcolesportfolio.com>
2009-03-08 16:35:27 +0100

    Créé méthodes modèles + utilise merb-action-args
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- app/controllers/instances.rb
+++ app/controllers/instances.rb
@@ -8,27 +8,44 @@ class Instances < Application
     display @instances
   end
 
-  def show
-
+  def show(id)
+    @instance = Instance.get(id)
   end
 
   def new
-
+    only_provides :html
+    @instance = Instance.new
+    display @instance
   end
 
-  def edit
-
+  def edit(id)
+    only_provides :html
+    @instance = Instance.get(id)
+    raise NotFound unless @instance
+    display @instance
   end
 
-  def create
+  def create(instance)
+    @instance = Instance.new(instance)
 
+    if @instance.save
+      redirect resource(@instance), :message => {:notice => _("Instance was successfully created")}
+    else
+      render :new
+    end
   end
 
-  def update
-
+  def update(id, instance)
+    @instance = Instance.get(id)
+    raise NotFound unless @instance
+    if @instance.update_attributes(instance)
+      redirect resource(@instance), :message => {:notice => _("Instance was successfully updated")}
+    else
+      display @instance, :edit
+    end
   end
 
-  def destroy
+  def destroy(id)
 
   end
 
