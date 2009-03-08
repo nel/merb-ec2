e8a74d307794f2838f5e154deb5ea52120b9464e
Alex Coles <alex@alexcolesportfolio.com>
2009-03-08 18:15:56 +0100

    Create JS for RESTful delete links
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,20 @@
 // Common JavaScript code across your application goes here.
+
+$(document).ready(function() {
+
+    $(".delete").click(function(e) {
+        if (confirm("Are you sure?")) {
+            var href = $(this).attr('href');
+            var href = href.substring(0, href.length - "/delete".length);
+
+            form = $("<form action='" + href + "' method='POST'>"
+              + "<input type='hidden' name='_method' value='DELETE'/>"
+              + "</form>"
+              ).appendTo("body");
+
+            form.submit();
+        }
+        return false;
+    });
+
+});
