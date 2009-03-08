573d7f17683de66defcda66a4e8704989e944643
Alex Coles <alex@alexcolesportfolio.com>
2009-03-08 16:27:25 +0100

    Rajouté instances resource à router.rb. Créé methodes contrôleur
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- app/controllers/instances.rb
+++ app/controllers/instances.rb
@@ -1,7 +1,41 @@
 class Instances < Application
 
+  before :load_instances
+
   def index
-    render
+    raw_instances = @ec2.describe_instances
+    @instances = raw_instances['reservationSet']['item']
+    display @instances
   end
-  
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def load_instances
+    @ec2 = Manager.new.instantiate
+  end
+
 end
