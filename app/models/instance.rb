953773a9722550e710cdc6319c5a2f872efa0cb5
Renaud (Nel) Morvan <renaud.morvan@feedback20.com>
2009-03-09 02:18:29 +0100

    fix: Instance.find_instances when no IM found refactor: remove amazon credentials from repository

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -2,9 +2,10 @@ class Instance
   class << self
 
     def find_instances(ec2)
-      raw_instances = ec2.describe_instances
-      instances = raw_instances['reservationSet']['item'].map { |i| i['instancesSet']['item'].first }
-      instances
+      raw_instances = ec2.describe_instances['reservationSet']
+      raw_instances ? 
+        raw_instances['item'].map { |i| i['instancesSet']['item'].first } :
+        []
     end
 
   end
